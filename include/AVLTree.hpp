3dcc2b19d4f67b59f6340ba9158eb028111d6b54
Onelig <onelig.dark@gmail.com>
2025-02-23 18:54:41 +0200

    Create class Node in class AVLTree

--- include/AVLTree.hpp
+++ include/AVLTree.hpp
@@ -5,7 +5,17 @@ namespace Tree
 	template<typename T, typename T_Height = unsigned char>
 	class AVLTree
 	{
-	public:
+	public: // Node
+		class Node
+		{
+		public:
+			T data;
+		private:
+			Node(T data = T(), Node* right = nullptr, Node* left = nullptr) : data(data), right(right), left(left), height(1) { }
 
+			Node* left;
+			Node* right;
+			T_Height height;
+		};
 	};
 }
